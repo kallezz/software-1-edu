2435c3ea0318c7d511833bd118e4266f1a2a0a66
kallezz <kalle.melender@hotmail.com>
2026-01-15 16:55:12 +0200

    module 4 task 6

--- module-4/task-6/pi.py
+++ module-4/task-6/pi.py
@@ -1,1 +1,20 @@
-# En ymmärtänyt tehtävänantoa...
+import random
+
+try:
+    points = int(input("How many points: "))
+except ValueError:
+    print("Invalid input")
+
+points_in_circle = 0
+
+# ToDo: Moodle haluaa while loop
+
+for i in range(points):
+    point = (random.uniform(-1, 1), random.uniform(-1, 1))
+
+    if point[0] ** 2 + point[1] ** 2 < 1:
+        points_in_circle += 1
+
+pi_approx = 4 * points_in_circle / points
+
+print(f"Approximation of pi: {pi_approx}")
